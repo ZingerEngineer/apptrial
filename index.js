7fda8d5abfb81847bee5a110ffef824d77e08c36
ZingerEngineer <ramyisez@gmail.com>
2021-09-28 14:08:41 +0200

    apptrial

---
+++ index.js
@@ -0,0 +1,22 @@
+// alert("HEY BROOO !!!");
+//const text = 'it isn\'t me';
+//console.log(text)
+// let x = 1 ;
+// while (x<=4) {
+
+// console.log("NO");
+//  x++;
+
+
+// }
+// document.write("LOL");
+// document.write("Ramy"+1)\
+var x = 1
+var y = 3
+var z = x+y
+for (z<=10; x++;)
+{
+    console.log(z)
+
+}
+console.log("youarebig")
